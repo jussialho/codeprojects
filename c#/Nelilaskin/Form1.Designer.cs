ee2547742f5c02f9e8d38c7e8b3754d2b666b31e
Jussi Alho <76096972+jussialho@users.noreply.github.com>
2021-01-15 12:23:48 +0200

    Add files via upload

---
+++ c#/Nelilaskin/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Nelilaskin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.luku1 = new System.Windows.Forms.TextBox();
+            this.tulos = new System.Windows.Forms.TextBox();
+            this.luku2 = new System.Windows.Forms.TextBox();
+            this.luku1Label = new System.Windows.Forms.Label();
+            this.luku2Label = new System.Windows.Forms.Label();
+            this.tulosLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.plus = new System.Windows.Forms.Button();
+            this.miinus = new System.Windows.Forms.Button();
+            this.kerto = new System.Windows.Forms.Button();
+            this.jako = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tyhjennäKentätToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.suljeOhjelmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // luku1
+            // 
+            this.luku1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luku1.Location = new System.Drawing.Point(35, 142);
+            this.luku1.Name = "luku1";
+            this.luku1.Size = new System.Drawing.Size(100, 30);
+            this.luku1.TabIndex = 0;
+            // 
+            // tulos
+            // 
+            this.tulos.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tulos.Location = new System.Drawing.Point(268, 187);
+            this.tulos.Name = "tulos";
+            this.tulos.Size = new System.Drawing.Size(234, 30);
+            this.tulos.TabIndex = 1;
+            // 
+            // luku2
+            // 
+            this.luku2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luku2.Location = new System.Drawing.Point(35, 232);
+            this.luku2.Name = "luku2";
+            this.luku2.Size = new System.Drawing.Size(100, 30);
+            this.luku2.TabIndex = 2;
+            // 
+            // luku1Label
+            // 
+            this.luku1Label.AutoSize = true;
+            this.luku1Label.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luku1Label.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.luku1Label.Location = new System.Drawing.Point(32, 114);
+            this.luku1Label.Name = "luku1Label";
+            this.luku1Label.Size = new System.Drawing.Size(74, 23);
+            this.luku1Label.TabIndex = 3;
+            this.luku1Label.Text = "Luku 1";
+            // 
+            // luku2Label
+            // 
+            this.luku2Label.AutoSize = true;
+            this.luku2Label.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luku2Label.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.luku2Label.Location = new System.Drawing.Point(32, 206);
+            this.luku2Label.Name = "luku2Label";
+            this.luku2Label.Size = new System.Drawing.Size(74, 23);
+            this.luku2Label.TabIndex = 4;
+            this.luku2Label.Text = "Luku 2";
+            // 
+            // tulosLabel
+            // 
+            this.tulosLabel.AutoSize = true;
+            this.tulosLabel.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tulosLabel.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.tulosLabel.Location = new System.Drawing.Point(265, 159);
+            this.tulosLabel.Name = "tulosLabel";
+            this.tulosLabel.Size = new System.Drawing.Size(74, 23);
+            this.tulosLabel.TabIndex = 5;
+            this.tulosLabel.Text = "TULOS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(222, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "=";
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.Location = new System.Drawing.Point(163, 97);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(38, 34);
+            this.plus.TabIndex = 7;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            // 
+            // miinus
+            // 
+            this.miinus.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miinus.Location = new System.Drawing.Point(163, 153);
+            this.miinus.Name = "miinus";
+            this.miinus.Size = new System.Drawing.Size(38, 34);
+            this.miinus.TabIndex = 8;
+            this.miinus.Text = "-";
+            this.miinus.UseVisualStyleBackColor = true;
+            // 
+            // kerto
+            // 
+            this.kerto.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kerto.Location = new System.Drawing.Point(163, 210);
+            this.kerto.Name = "kerto";
+            this.kerto.Size = new System.Drawing.Size(38, 34);
+            this.kerto.TabIndex = 9;
+            this.kerto.Text = "x";
+            this.kerto.UseVisualStyleBackColor = true;
+            // 
+            // jako
+            // 
+            this.jako.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jako.Location = new System.Drawing.Point(163, 268);
+            this.jako.Name = "jako";
+            this.jako.Size = new System.Drawing.Size(38, 34);
+            this.jako.TabIndex = 10;
+            this.jako.Text = "/";
+            this.jako.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label1.Location = new System.Drawing.Point(157, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 33);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "NELILASKIN";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tyhjennäKentätToolStripMenuItem,
+            this.suljeOhjelmaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(536, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tyhjennäKentätToolStripMenuItem
+            // 
+            this.tyhjennäKentätToolStripMenuItem.Name = "tyhjennäKentätToolStripMenuItem";
+            this.tyhjennäKentätToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.tyhjennäKentätToolStripMenuItem.Text = "Tyhjennä kentät";
+            this.tyhjennäKentätToolStripMenuItem.Click += new System.EventHandler(this.TyhjennaKentatToolStripMenuItem_Click);
+            // 
+            // suljeOhjelmaToolStripMenuItem
+            // 
+            this.suljeOhjelmaToolStripMenuItem.Name = "suljeOhjelmaToolStripMenuItem";
+            this.suljeOhjelmaToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.suljeOhjelmaToolStripMenuItem.Text = "Sulje ohjelma";
+            this.suljeOhjelmaToolStripMenuItem.Click += new System.EventHandler(this.SuljeOhjelmaToolStripMenuItem_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(269, 239);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(196, 23);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Kirjoita tulos tiedostoon";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 321);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.jako);
+            this.Controls.Add(this.kerto);
+            this.Controls.Add(this.miinus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tulosLabel);
+            this.Controls.Add(this.luku2Label);
+            this.Controls.Add(this.luku1Label);
+            this.Controls.Add(this.luku2);
+            this.Controls.Add(this.tulos);
+            this.Controls.Add(this.luku1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NELILASKIN";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox luku1;
+        private System.Windows.Forms.TextBox tulos;
+        private System.Windows.Forms.TextBox luku2;
+        private System.Windows.Forms.Label luku1Label;
+        private System.Windows.Forms.Label luku2Label;
+        private System.Windows.Forms.Label tulosLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button miinus;
+        private System.Windows.Forms.Button kerto;
+        private System.Windows.Forms.Button jako;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tyhjennäKentätToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem suljeOhjelmaToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
